f24fd15f98f4ac82a201a2aa2aa959ebdb73eea5
Matthew Teran <matt@Matthews-Air.lan1>
2020-04-21 22:37:32 -0500

    Added some styling to the article page, including a sidebar

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,10 @@
 document.querySelector('#hamburger').onclick = () => {
     console.log('f')
     if (document.querySelector('#menu').style.display === 'none'){
-        document.querySelector('#menu').style.display = 'block'
+        document.querySelector('#menu').style.display = 'block';
+        document.querySelector('#header-divider').style.display = 'none';
     } else {
         document.querySelector('#menu').style.display = 'none'
+        document.querySelector('#header-divider').style.display = 'block';
     }
 }
