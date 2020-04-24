888ab0fc088a966df4db08b910db0ebb02f18db7
Matthew Teran <matt@Matthews-Air.lan1>
2020-04-24 17:18:15 -0500

    Added a 'Content' tab on the sidebar of articles

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,7 @@
 document.querySelector('#hamburger').onclick = () => {
-    console.log('f')
     if (document.querySelector('#menu').style.display === 'none'){
         document.querySelector('#menu').style.display = 'block';
-        document.querySelector('#header-divider').style.display = 'none';
     } else {
         document.querySelector('#menu').style.display = 'none'
-        document.querySelector('#header-divider').style.display = 'block';
     }
 }
